ec988fd15f9e38b30d2675ef59e197b21fce685a
cephalin <cephalin@microsoft.com>
2015-02-06 14:50:48 -0600

    add all files

---
+++ SVGPrep/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SVGPrep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonOpen = new System.Windows.Forms.Button();
+            this.ButtonSaveAs = new System.Windows.Forms.Button();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.ALinksGrid = new System.Windows.Forms.DataGridView();
+            this.ButtonDeleteTitles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HRef = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Desc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TabIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InATag = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.ALinksGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonOpen
+            // 
+            this.ButtonOpen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonOpen.Location = new System.Drawing.Point(1, 1);
+            this.ButtonOpen.Margin = new System.Windows.Forms.Padding(2);
+            this.ButtonOpen.Name = "ButtonOpen";
+            this.ButtonOpen.Size = new System.Drawing.Size(103, 28);
+            this.ButtonOpen.TabIndex = 1;
+            this.ButtonOpen.Text = "Open SVG File";
+            this.ButtonOpen.UseVisualStyleBackColor = true;
+            this.ButtonOpen.Click += new System.EventHandler(this.ButtonOpen_Click);
+            // 
+            // ButtonSaveAs
+            // 
+            this.ButtonSaveAs.Enabled = false;
+            this.ButtonSaveAs.Location = new System.Drawing.Point(109, 1);
+            this.ButtonSaveAs.Name = "ButtonSaveAs";
+            this.ButtonSaveAs.Size = new System.Drawing.Size(90, 28);
+            this.ButtonSaveAs.TabIndex = 2;
+            this.ButtonSaveAs.Text = "Save As";
+            this.ButtonSaveAs.UseVisualStyleBackColor = true;
+            this.ButtonSaveAs.Click += new System.EventHandler(this.ButtonSaveAs_Click);
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Enabled = false;
+            this.ButtonSave.Location = new System.Drawing.Point(205, 1);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(79, 27);
+            this.ButtonSave.TabIndex = 3;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            this.ButtonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // ALinksGrid
+            // 
+            this.ALinksGrid.AllowUserToAddRows = false;
+            this.ALinksGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ALinksGrid.ColumnHeadersVisible = false;
+            this.ALinksGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.HRef,
+            this.Title,
+            this.Desc,
+            this.TabIndex,
+            this.InATag,
+            this.Delete});
+            this.ALinksGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ALinksGrid.Location = new System.Drawing.Point(0, 35);
+            this.ALinksGrid.Name = "ALinksGrid";
+            this.ALinksGrid.RowHeadersVisible = false;
+            this.ALinksGrid.Size = new System.Drawing.Size(866, 348);
+            this.ALinksGrid.TabIndex = 4;
+            // 
+            // ButtonDeleteTitles
+            // 
+            this.ButtonDeleteTitles.Location = new System.Drawing.Point(740, 1);
+            this.ButtonDeleteTitles.Name = "ButtonDeleteTitles";
+            this.ButtonDeleteTitles.Size = new System.Drawing.Size(114, 28);
+            this.ButtonDeleteTitles.TabIndex = 5;
+            this.ButtonDeleteTitles.Text = "Delete all other titles";
+            this.ButtonDeleteTitles.UseVisualStyleBackColor = true;
+            this.ButtonDeleteTitles.Click += new System.EventHandler(this.ButtonDeleteTitles_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Every SVG file is automatically processed, such as adding cursor, target, and tab" +
+    "index attributes";
+            // 
+            // ID
+            // 
+            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 5;
+            // 
+            // HRef
+            // 
+            this.HRef.HeaderText = "HRef";
+            this.HRef.Name = "HRef";
+            this.HRef.ReadOnly = true;
+            this.HRef.Width = 200;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.Width = 200;
+            // 
+            // Desc
+            // 
+            this.Desc.HeaderText = "Description";
+            this.Desc.Name = "Desc";
+            this.Desc.Width = 200;
+            // 
+            // TabIndex
+            // 
+            this.TabIndex.HeaderText = "TabIndex";
+            this.TabIndex.Name = "TabIndex";
+            this.TabIndex.Width = 77;
+            // 
+            // InATag
+            // 
+            this.InATag.HeaderText = "Nested?";
+            this.InATag.Name = "InATag";
+            this.InATag.ReadOnly = true;
+            this.InATag.Width = 53;
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "Delete?";
+            this.Delete.Name = "Delete";
+            this.Delete.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 383);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.ButtonDeleteTitles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ALinksGrid);
+            this.Controls.Add(this.ButtonSaveAs);
+            this.Controls.Add(this.ButtonOpen);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "SVG Prep Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ALinksGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonOpen;
+        private System.Windows.Forms.Button ButtonSaveAs;
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.DataGridView ALinksGrid;
+        private System.Windows.Forms.Button ButtonDeleteTitles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewLinkColumn HRef;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Desc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TabIndex;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn InATag;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Delete;
+    }
+}
+
---
+++ SVGPrep/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+
+namespace SVGPrep
+{
+    public partial class Form1 : Form
+    {
+        XDocument svg;
+        string currentFile = "";
+        bool isGridDirty = false;
+
+        string formTitle = "SVG Prep";
+        XNamespace ns = "http://www.w3.org/2000/svg";
+        XNamespace xlink = "http://www.w3.org/1999/xlink";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// close the form only after checking with the user about whether they want to save the file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Form1_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            // Check if current file is dirty
+            if (isGridDirty)
+            {
+                if (MessageBox.Show("You have unsaved changes in the current SVG file. If you click OK, any unsaved changes will be discarded",
+                                    "Unsaved Changes",
+                                    MessageBoxButtons.OKCancel) != DialogResult.OK)
+                    e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// open an SVG file, process it, and populate the grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonOpen_Click(object sender, EventArgs e)
+        {
+            // Check if current file is dirty
+            if (isGridDirty)
+            {
+                if (MessageBox.Show("You have unsaved changes in the current SVG file. If you click OK, any unsaved changes will be discarded",
+                                    "Unsaved Changes",
+                                    MessageBoxButtons.OKCancel) != DialogResult.OK)
+                    return;
+            }
+
+            ALinksGrid.Visible = false;
+            // Open file
+
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            openFile1.Filter = "SVG Files|*.svg";
+            if (openFile1.ShowDialog() != DialogResult.OK)
+                return;
+
+            svg = XDocument.Load(openFile1.FileName);
+
+            // Process SVG DOM and populate the grid
+
+            ALinksGrid.AutoGenerateColumns = false;
+            ALinksGrid.Rows.Clear();
+            foreach (XElement alink in svg.Descendants(ns + "a"))
+            {
+                alink.SetAttributeValue("cursor", "pointer");   // set cursor="pointer"
+                alink.SetAttributeValue("target", "_blank");    // set target="_blank"
+                if(alink.Attribute("tabindex") == null)         // set tabindex="0" if not exist
+                    alink.SetAttributeValue("tabindex", "0");
+
+                // create <title> if not exist in decendents
+
+                XElement title = null;
+                if(alink.Descendants(ns + "title").Count() > 0)
+                    title = alink.Descendants(ns + "title").First();
+                if ( title == null)
+                {
+                    title = new XElement(ns + "title", "");
+                    alink.Add(title);
+                }
+
+                // create <desc> if not exist in descendents
+
+                XElement desc = null;
+                if (alink.Descendants(ns + "desc").Count() > 0)
+                    desc = alink.Descendants(ns + "desc").First();
+                if (desc == null)
+                {
+                    desc = new XElement(ns + "desc", "");
+                    alink.Add(desc);
+                }
+
+                // add ids for <title> and <desc> if not exist
+
+                string titleid, descid;
+                if (title.Attribute("id") == null)
+                {
+                    titleid = GenerateId();
+                    title.SetAttributeValue("id", titleid);
+                }
+                else titleid = title.Attribute("id").Value;
+
+                if (desc.Attribute("id") == null)
+                {
+                    descid = GenerateId();
+                    desc.SetAttributeValue("id", descid);
+                }
+                else descid = desc.Attribute("id").Value;
+
+                alink.SetAttributeValue("aria-labelledby", titleid + " " + descid); // set aria-labelledby="[titleid] [descid]"
+
+                // create a new row for the link in the grid
+
+                ALinksGrid.Rows.Add(
+                    alink.DescendantsAndSelf().First(el => el.Attribute("id") != null).Attribute("id").Value,   // id
+                    alink.Attribute(xlink + "href").Value,                                                      // href
+                    alink.Descendants(ns + "title").First().Value,                                              // title
+                    alink.Descendants(ns + "desc").First().Value,                                               // desc
+                    alink.Attribute("tabindex").Value,                                                          // tabindex
+                    alink.Ancestors(ns + "a").Count() > 0 ? true : false,                                       // nested?
+                    false);                                                                                     // delete link? (no by default)
+            }
+            ALinksGrid.Refresh();
+            ALinksGrid.ColumnHeadersVisible = true;
+            ALinksGrid.Visible = true;
+            ALinksGrid.CellValueChanged += ALinksGrid_CellValueChanged;
+
+            currentFile = openFile1.FileName;
+            ButtonSave.Enabled = true;
+            ButtonSaveAs.Enabled = true;
+            this.Text = String.Format("{0} - {1}", formTitle, currentFile);
+            isGridDirty = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Save As button clicked. update the SVG DOM and the grid and save to a user-defined file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "SVG files (*.svg)|*.svg";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try 
+                {
+                    UpdateData();   // update SVG DOM and grid
+
+                    Stream output = saveFileDialog1.OpenFile();
+                    if (output != null)
+                    {
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.Indent = true;
+                        settings.NewLineOnAttributes = true;
+
+                        XmlWriter writer = XmlWriter.Create(output, settings);
+                        svg.Save(writer);
+                        writer.Flush();
+                        output.Close();
+                    }
+
+                    currentFile = saveFileDialog1.FileName;
+                    this.Text = String.Format("{0} - {1}", formTitle, currentFile);
+                    isGridDirty = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving the file. Try saving it in another file.\n" + ex.Message);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Save button clicked. update the SVG DOM and the grid and save to a user-defined file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (isGridDirty)
+            {
+                try
+                {
+                    UpdateData();   // update SVG DOM and grid
+
+                    Stream output = File.OpenWrite(currentFile);
+                    if (output != null)
+                    {
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.Indent = true;
+                        settings.NewLineOnAttributes = true;
+
+                        XmlWriter writer = XmlWriter.Create(output, settings);
+                        svg.Save(writer);
+                        writer.Flush();
+                        output.Close();
+                    }
+
+                    isGridDirty = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving the file. Try saving it in another file.\n" + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// the user changed something in the grid. keep track of change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void ALinksGrid_CellValueChanged(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+            isGridDirty = true;
+        }
+
+        /// <summary>
+        /// update the SVG DOM and the grid
+        /// </summary>
+        void UpdateData()
+        {
+            var alinks = svg.Descendants(ns + "a");
+            ALinksGrid.CellValueChanged -= ALinksGrid_CellValueChanged;
+            List<DataGridViewRow> toDelete = new List<DataGridViewRow>();
+
+            foreach (DataGridViewRow row in ALinksGrid.Rows)
+            {
+                XElement alink = svg.Descendants().First(el => (el.Attribute("id") != null) && (el.Attribute("id").Value == (string)row.Cells["ID"].Value));
+                if (alink.Name.LocalName != "a")
+                    alink = alink.Parent;
+                if (alink == null)
+                    throw new Exception(String.Format("Cannot find the <a> tag for the current row with href=\"{0}\"", row.Cells["id"].Value));
+
+                // delete <a> from SVG DOM if specified, including any <title> or <desc> in it
+                if ((bool)row.Cells["Delete"].Value)
+                {
+                    XElement parent = alink.Parent;
+                    alink.Remove();
+                    foreach (XElement title in alink.Elements(ns + "title"))
+                        title.Remove();
+                    foreach (XElement desc in alink.Elements(ns + "desc"))
+                        desc.Remove();
+                    parent.Elements().ElementAt(alink.ElementsBeforeSelf().Count()).AddBeforeSelf(alink.Elements());    //add the <a>'s children back
+                    toDelete.Add(row);
+                    continue;
+                }
+
+                alink.Descendants(ns + "title").First().Value = (string)row.Cells["Title"].Value;   // update <title>
+                alink.Descendants(ns + "desc").First().Value = (string)row.Cells["Desc"].Value;     // update <desc>
+                alink.Attribute("tabindex").Value = (string)row.Cells["TabIndex"].Value;            // update tabindex="#"
+
+            }
+
+            // remove deleted links from the grid
+
+            ALinksGrid.Visible = false;
+            foreach (var row in toDelete)
+                ALinksGrid.Rows.Remove(row);
+            ALinksGrid.Refresh();
+            ALinksGrid.Visible = true;
+
+            ALinksGrid.CellValueChanged += ALinksGrid_CellValueChanged;
+        }
+
+        /// <summary>
+        /// when the button is clicked, this method deletes all title and desc tags in the SVG that are not already shown in the grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonDeleteTitles_Click(object sender, EventArgs e)
+        {
+            List<XElement> goodTitles = new List<XElement>();
+            foreach (XElement alink in svg.Descendants(ns + "a"))
+            {
+                goodTitles.Add(alink.Descendants(ns + "title").First());
+            }
+
+            IEnumerable<XElement> titles = svg.Descendants(ns + "title");
+            int i = titles.Count();
+            while(i-- > 0)
+            {
+                XElement title = titles.ElementAt(i);
+                if (!goodTitles.Contains(title))
+                {
+                    XElement desc = title.Parent.Element(ns + "desc");
+                    title.Remove();
+                    if (desc != null)
+                        desc.Remove();
+                }
+                else
+                {
+                    //debug
+                }
+            }
+        }
+
+        /// <summary>
+        /// generate a unique ID in the current SVG
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateId()
+        {
+            IEnumerable<XAttribute> ids = svg.Descendants().Where(x => x.Attribute("id") != null ).Attributes("id");
+            string newId = "sp-";
+            while (true)
+            {
+                newId += Guid.NewGuid().ToString("N").Substring(0, 5);
+                foreach (XAttribute id in ids)
+                {
+                    if (id.Value == newId)
+                        continue;
+                }
+                break;
+            }
+            return newId;
+        }
+    }
+
+}
